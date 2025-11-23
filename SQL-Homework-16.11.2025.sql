362585a38d47d0a3e4d0e8f2142d968aa35483c9
AlexPr06 <freezay00828002@gmail.com>
2025-11-23 20:41:20 +0200

    2. All three requests are done.

--- SQL-Homework-16.11.2025.sql
+++ SQL-Homework-16.11.2025.sql
@@ -1,6 +1,23 @@
 
 -- Query 1
+SELECT 
+	id                        AS [Employee's id],
+	name                      AS [Employee name],
+	ISNULL(salary, 0)         AS [Month salary],
+	ISNULL(salary, 0) * 12    AS [Year salary]
+FROM employees;
+
 
 -- Query 2
+SELECT DISTINCT
+	department_id             AS [Unique department ids]
+FROM employees;
+
 
 -- Query 3
+
+SELECT DISTINCT
+	id,
+	hire_date                                  AS [Unique department ids],
+	(DATEDIFF(YEAR, hire_date, GETDATE()))     AS [How much years is working]
+FROM employees;
